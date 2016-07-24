ba86cb5de86a26a11c5a487a9f10c3abd6618915
Simeon Visser <simeon87@gmail.com>
2016-07-24 23:46:54 +0100

    Refactored code to calculate pixel colors first before starting to render pixels

--- src/Main.purs
+++ src/Main.purs
@@ -5,10 +5,15 @@ import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Data.Array ((..))
 import Data.Int (floor, toNumber)
 import Data.Maybe (Maybe(Just, Nothing))
+import Data.Ord ((<))
 import Data.Tuple (Tuple(..))
 import Graphics.Canvas (CANVAS, Context2D, getCanvasElementById, getContext2D, setCanvasDimensions)
-import Graphics.Canvas.Free (fill, rect, runGraphics, setFillStyle)
-import Prelude (Unit, bind, pure, ($))
+import Graphics.Canvas.Free (fillRect, runGraphics, setFillStyle)
+import Prelude (Unit, bind, map, pure, ($))
+
+type Pixel = Tuple Number Number
+type PixelColor = String
+type RenderedPixel = Tuple Pixel PixelColor
 
 canvasWidth :: Number
 canvasWidth = 800.0
@@ -16,23 +21,24 @@ canvasWidth = 800.0
 canvasHeight :: Number
 canvasHeight = 600.0
 
-calculatePixel :: Number -> Number -> Boolean
-calculatePixel x y = true
+calculatePixelColor :: Pixel -> PixelColor
+calculatePixelColor (Tuple x y) = if x < 30.0 then "red" else "white"
 
-renderPixel :: forall e. Context2D -> Number -> Number -> Eff (canvas :: CANVAS | e) Unit
-renderPixel context x y = do
-  runGraphics context $ do
-    let pixelColor = if calculatePixel x y then "black" else "white"
-    setFillStyle pixelColor
-    rect { x: x, y: y, w: 1.0, h: 1.0 }
-    fill
+renderPixels :: forall e. Context2D -> Array RenderedPixel -> Eff (canvas :: CANVAS | e) Unit
+renderPixels context rpixels = do
+  foreachE rpixels $ \(Tuple (Tuple x y) color) ->
+    runGraphics context $ do
+      setFillStyle color
+      fillRect { x: x, y: y, w: 1.0, h: 1.0 }
 
-pixels :: Array (Tuple Number Number)
+pixels :: Array Pixel
 pixels = do
   x <- 0 .. (floor canvasWidth)
   y <- 0 .. (floor canvasHeight)
   pure (Tuple (toNumber x) (toNumber y))
 
+pixelColors :: Array Pixel -> Array RenderedPixel
+pixelColors = map (\px -> (Tuple px (calculatePixelColor px)))
 
 main :: forall e. Eff (canvas :: CANVAS, console :: CONSOLE | e) Unit
 main = do
@@ -43,5 +49,4 @@ main = do
     Just canvas -> do
       setCanvasDimensions { width: canvasWidth, height: canvasHeight } canvas
       context <- getContext2D canvas
-      foreachE pixels $ \(Tuple x y) ->
-        renderPixel context x y
+      renderPixels context (pixelColors pixels)
